0079717fc0902ffbb001f6ba56862fadde01fbbf
tuposilwe <rudigermkondya@gmail.com>
2023-09-22 08:07:25 -0700

    Refactor- create lookup hooks

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,7 +8,7 @@ import {
   Spinner,
   Text,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenre";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
@@ -16,10 +16,7 @@ interface Props {
   selectedGenreId?: number;
 }
 
-const GenreList = ({
-  selectedGenreId,
-  onSelectGenre,
-}: Props) => {
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,22 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import useGenres from "./useGenres";
 
-const apiClient = new APIClient<Genre>('/genres');
+const useGenre =(id?:number) =>{
+    const { data: genres } = useGenres();
+    return genres?.results.find((g) => g.id === id);
 
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
 }
 
-const useGenres = () =>
-  useQuery({
-    queryKey: ["genres"],
-    queryFn:apiClient.getAll,
-    staleTime:24*60*60*1000, //24h
-    initialData:{count:genres.length,results:genres}
-  });
-
-export default useGenres;
+export default useGenre;
